cdd08e32067fd72c445b99d6c0864208df9af0f3
Burak Yeşilyurt <budidak@pm.me>
2024-09-24 19:51:15 +0300

    Add more photos and add onblur and onfocus events

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-/*Name this external file gallery.js*/
 let image = document.getElementById("image");
 
+let images = document.getElementsByClassName("preview");
+for (let img of images) {
+  img.setAttribute("tabindex", "0");
+}
+
 function upDate(previewPic) {
-  /* In this function you should 
-     1) change the url for the background image of the div with the id = "image" 
-     to the source file of the preview image
-     
-     2) Change the text  of the div with the id = "image" 
-     to the alt text of the preview image 
-     */
   let activeSrc = previewPic.getAttribute("src");
   let activeText = previewPic.getAttribute("alt");
   image.style["background-image"] = `url('${activeSrc}')`;
@@ -16,13 +13,6 @@ function upDate(previewPic) {
 }
 
 function unDo() {
-  /* In this function you should 
- 1) Update the url for the background image of the div with the id = "image" 
- back to the orginal-image.  You can use the css code to see what that original URL was
- 
- 2) Change the text  of the div with the id = "image" 
- back to the original text.  You can use the html code to see what that original text was
- */
   image.style["background-image"] = "url('')";
   image.style.backgroundColor = "#8e68ff";
   image.textContent = "Hover over an image below to display here.";
